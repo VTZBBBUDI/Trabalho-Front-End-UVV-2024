2f67cae2289598ff7a5cb4d2d3e0badc6b2a96b8
Arthur Fernandes <artyarthur3@gmail.com>
2024-11-19 17:14:32 -0300

    pagina-home-feita

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -1,22 +1,37 @@
 const imageContainers = document.querySelectorAll('.image-container');
-    
-imageContainers.forEach(container => {
-  container.addEventListener('mouseenter', () => {
-    const overlay = container.querySelector('.overlay');
-    overlay.classList.remove('opacity-0');
-    overlay.classList.add('opacity-100');
-    
-    // Expandir apenas no eixo Y para evitar overflow no eixo X
-    container.style.transform = "scaleY(1.1)";
-    container.style.transition = "transform 0.3s ease-in-out";
-  });
+const contatoBtn = document.getElementById('cttBtn');
 
-  container.addEventListener('mouseleave', () => {
-    const overlay = container.querySelector('.overlay');
-    overlay.classList.remove('opacity-100');
-    overlay.classList.add('opacity-0');
-    
-    // Voltar para o tamanho original
-    container.style.transform = "scaleY(1)";
+const darVidaAsImagens = () => {
+  imageContainers.forEach((container, index) => {
+    container.addEventListener('mouseenter', () => {
+      const overlay = container.querySelector('.overlay');
+      overlay.classList.remove('opacity-0');
+      overlay.classList.add('opacity-100');
+      container.style.transform = "scaleY(1.1)";
+      container.style.transition = "transform 0.3s ease-in-out";
+    });
+
+    container.addEventListener('mouseleave', () => {
+      const overlay = container.querySelector('.overlay');
+      overlay.classList.remove('opacity-100');
+      overlay.classList.add('opacity-0');
+      container.style.transform = "scaleY(1)";
+    });
+
+    container.addEventListener('click', () => {
+      const link = container.getAttribute('data-link');
+      if (link) {
+        window.location.href = link;
+      }
+    });
+
+    contatoBtn.addEventListener('click', () => {
+      const link = contatoBtn.getAttribute('data-link');
+      if (link) {
+        window.location.href = link;
+      }
+    });
   });
-});
+};
+
+darVidaAsImagens();
